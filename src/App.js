825ea0daf7f0b98f4b2cb9be628a4768e86d1b3b
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-08-16 16:56:39 +0530

    add darklight mode btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 import WeatherDashboard from './components/WeatherDashboard';
 
 import './App.css';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
+
   return (
     <div>
+      {/* <WeatherDashboard /> */}
+
+      <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+      <button onClick={toggleDarkMode} className="toggle-button">
+        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </button>
       <WeatherDashboard />
+    </div>
+
+
     </div>
   );
 }
